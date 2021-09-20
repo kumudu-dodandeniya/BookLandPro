d3d5acfbc1ca204df950b5b756c7f77052ee861a
Kumudu Dodandeniya <it20089672@my.sliit.lk>
2021-09-20 22:38:23 +0530

    add book

--- app/src/main/java/com/example/mobileapp/AdminCategory.java
+++ app/src/main/java/com/example/mobileapp/AdminCategory.java
@@ -1,14 +1,14 @@
 package com.example.mobileapp;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class cardCRUD extends AppCompatActivity {
+import android.os.Bundle;
+
+public class AdminCategory extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_card_crud);
+        setContentView(R.layout.activity_admin_category);
     }
 }
